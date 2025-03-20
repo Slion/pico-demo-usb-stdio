37ec9dbb6385b10a99678e61929453ced2be130a
Stéphane Lenclud <github@lenclud.com>
2025-03-20 13:20:30 +0100

    Add delay on boot

--- pico_demo_usb_stdio.c
+++ pico_demo_usb_stdio.c
@@ -7,15 +7,17 @@
  #include "pico/stdlib.h"
 
  int main() {
-     stdio_init_all();
-
-     printf("pico_demo_usb_stdio\n");
-     while (true) {
-         // Read incoming characters and write them back
-         int input = stdio_getchar_timeout_us(1000);
-         if (input!=PICO_ERROR_TIMEOUT)
-         {
-             printf("%c",input);
-         }
-     }
+    stdio_init_all();
+    // Wait a bit to allow host port monitor to connect
+    sleep_ms(1000);
+    //
+    printf("pico_demo_usb_stdio\n");
+    while (true) {
+        // Read incoming characters and write them back
+        int input = stdio_getchar_timeout_us(1000);
+        if (input!=PICO_ERROR_TIMEOUT)
+        {
+            printf("%c",input);
+        }
+    }
  }
